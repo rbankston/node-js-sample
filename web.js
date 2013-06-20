417429d3b42037244a7b1ec4155ffe7b7a81e29d
Ubuntu <ubuntu@ip-10-40-122-179.ec2.internal>
2013-06-20 06:00:51 +0000

    Updated the response

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('This is only a test. By the way. Hello World!');
 });
 
 var port = process.env.PORT || 5000;
