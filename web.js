3352d5bdabaa2c5259fd78a9be3b387c9c649c56
Ralph Bankston <ralph.l.bankston@gmail.com>
2013-06-20 06:12:39 +0000

    Playing with node some more

--- web.js
+++ web.js
@@ -10,6 +10,10 @@ app.get('/more', function(request, response) {
 	response.send('This is to get more');
 });
 
+app.get('/again', function(request, response) {
+	response.send('Testing some things again');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
